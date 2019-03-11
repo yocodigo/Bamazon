306ee75013d7e1a58b6a98b30f898bfc742aeacf
unknown <jjhernandez94@gmail.com>
2019-03-11 15:35:14 -0700

    UPDATE
    Prettier reformatting

--- javascript/bamazonCustomer.js
+++ javascript/bamazonCustomer.js
@@ -16,7 +16,7 @@ var connection = mysql.createConnection({
 });
 
 connection.connect(function(err) {
-  if (err) throw err;  
+  if (err) throw err;
   viewProducts();
 });
 
@@ -26,12 +26,8 @@ function viewProducts() {
     connection.query(query, function(err, res) {
         console.log("\n");
 
-
         console.log("ID" + " |        " + "PRODUCT NAME" + "       |       " + "DEPARTMENT NAME" + "      | " + "PRICE" + " | " + "STOCK QUANTITY");
         console.log("------------------------------------------------------------------------------------");
-        // for (var i = 0; i < res.length; i++) {
-            // console.log("id: " + res[i].id + "--[" + "Product Name: " + res[i].product_name + "]---------[" + "Department Name: " + 
-            // res[i].department_name + "]---------[" + "Price: " + res[i].price + "]---------[" + "Stock Quantity: " + res[i].stock_quantity + "]");
             console.log(res[0].id + " | " +  res[0].product_name + "              | " + res[0].department_name + "            | " + res[0].price + " |      " + res[0].stock_quantity);
             console.log(res[1].id + " | " +  res[1].product_name + "           | " + res[1].department_name + "            |   " + res[1].price + "   |      " + res[1].stock_quantity);
             console.log(res[2].id + " | " +  res[2].product_name + "     | " + res[2].department_name + "             |   " + res[2].price + "  |      " + res[2].stock_quantity);
@@ -48,47 +44,60 @@ function viewProducts() {
 }
 //Bamazon will first give you a choice to pick the ID of the product, then ask you to select the number of units for the product
 function itemSelection() {
-    inquirer
-        .prompt([
-            {
-                //The first should ask them the ID of the product they would like to buy.    
-                type: "input",
-                name: "item",
-                message: "Please enter the ID of the product",
-                choices: ['11','12','13','14','15','16','17','18','19','20']
-            },
-            {
-                type: "input",
-                name: "unit",
-                message: "Enter how many units you want"
-            }
-        ])
-        .then(function(answer) {
-        var query = "SELECT * FROM products WHERE id =" + answer.item; //query table products, stock_quantity
-        var updatedQuantity;
-        connection.query(query, function(err, res) {
-            if (answer.unit <= res[0].stock_quantity) {
-                updatedQuantity = res[0].stock_quantity - answer.unit;
-                console.log("\n" + "\n" + "\n" + "\n" + "\n" + "\n" + "\n" + "\n" + "\n" + "\n");
-                console.log("-------------------------------------------");
-                console.log(">>>>>>>>>> YOUR BALANCE: $" + (answer.unit * res[0].price)+ " <<<<<<<<<<<");
-                console.log("-------------------------------------------");                                                
-                var item = answer.item;
-                updateStock(updatedQuantity, item);
-            }
-            else {
-                return console.log("Insufficient quantity!");
-            }        
-        });
-        //Updates stock quantity
-           
-        });
-    }
+  inquirer
+    .prompt([
+      {
+        //The first should ask them the ID of the product they would like to buy.
+        type: "input",
+        name: "item",
+        message: "Please enter the ID of the product",
+        choices: ["11", "12", "13", "14", "15", "16", "17", "18", "19", "20"]
+      },
+      {
+        type: "input",
+        name: "unit",
+        message: "Enter how many units you want"
+      }
+    ])
+    .then(function(answer) {
+      var query = "SELECT * FROM products WHERE id =" + answer.item; //query table products, stock_quantity
+      var updatedQuantity;
+      connection.query(query, function(err, res) {
+        if (answer.unit <= res[0].stock_quantity) {
+          updatedQuantity = res[0].stock_quantity - answer.unit;
+          console.log(
+            "\n" + "\n" + "\n" + "\n" + "\n" + "\n" + "\n" + "\n" + "\n" + "\n"
+          );
+          console.log("-------------------------------------------");
+          console.log(
+            ">>>>>>>>>> YOUR BALANCE: $" +
+              answer.unit * res[0].price +
+              " <<<<<<<<<<<"
+          );
+          console.log("-------------------------------------------");
+          var item = answer.item;
+          updateStock(updatedQuantity, item);
+        } else {
+          return console.log("Insufficient quantity!");
+        }
+      });
+      //Updates stock quantity
+    });
+}
 
-    function updateStock(updatedQuantity, item) {
-        var query = "UPDATE products SET stock_quantity = " + "'" + updatedQuantity + "' " + "WHERE id =" + " '" + item + "'" + ";";
-        connection.query(query, function(err, res) {
-            console.log("What would else would you like to buy?");
-            viewProducts();                                
-        });     
-    }    
+function updateStock(updatedQuantity, item) {
+  var query =
+    "UPDATE products SET stock_quantity = " +
+    "'" +
+    updatedQuantity +
+    "' " +
+    "WHERE id =" +
+    " '" +
+    item +
+    "'" +
+    ";";
+  connection.query(query, function(err, res) {
+    console.log("What would else would you like to buy?");
+    viewProducts();
+  });
+}
--- javascript/working_bamazonCustomer_with_switch.js
+++ javascript/working_bamazonCustomer_with_switch.js
@@ -14,105 +14,122 @@ var connection = mysql.createConnection({
 });
 
 connection.connect(function(err) {
-  if (err) throw err;  
+  if (err) throw err;
 });
 
 //Populate this database with around 10 different products. (i.e. Insert "mock" data rows into this database and table).
 var query = "SELECT * FROM products";
 connection.query(query, function(err, res) {
-    for (var i = 0; i < res.length; i++) {
-        console.log("id: " + res[i].id + "--[" + "Product Name: " + res[i].product_name + "]---------[" + "Department Name: " + 
-        res[i].department_name + "]---------[" + "Price: " + res[i].price + "]---------[" + "Stock Quantity: " + res[i].stock_quantity + "]");
-    }
-    console.log('\n');
-    itemSelection();
+  for (var i = 0; i < res.length; i++) {
+    console.log(
+      "id: " +
+        res[i].id +
+        "--[" +
+        "Product Name: " +
+        res[i].product_name +
+        "]---------[" +
+        "Department Name: " +
+        res[i].department_name +
+        "]---------[" +
+        "Price: " +
+        res[i].price +
+        "]---------[" +
+        "Stock Quantity: " +
+        res[i].stock_quantity +
+        "]"
+    );
+  }
+  console.log("\n");
+  itemSelection();
 });
 
 //The app should then prompt users with two messages.
 function itemSelection() {
-    inquirer
-        .prompt([
-            {
-                //The first should ask them the ID of the product they would like to buy.    
-                type: "input",
-                name: "item",
-                message: "Please enter the ID of the product",
-                choices: ['11','12','13','14','15','16','17','18','19','20']
-            },
-            {
-                type: "input",
-                name: "unit",
-                message: "Enter how many units you want"
-            }
-        ])
-        .then(function(answer) {
-            switch(answer.item) {
-                case '11':
-                    item11(answer.unit);
-                    break;
-                
-                case '12':
-                    item12(answer.unit);
-                    break;    
-                
-                case '13':
-                    item13(answer.unit);
-                    break;
-                
-                case '14':
-                    item14(answer.unit);
-                    break;
+  inquirer
+    .prompt([
+      {
+        //The first should ask them the ID of the product they would like to buy.
+        type: "input",
+        name: "item",
+        message: "Please enter the ID of the product",
+        choices: ["11", "12", "13", "14", "15", "16", "17", "18", "19", "20"]
+      },
+      {
+        type: "input",
+        name: "unit",
+        message: "Enter how many units you want"
+      }
+    ])
+    .then(function(answer) {
+      switch (answer.item) {
+        case "11":
+          item11(answer.unit);
+          break;
 
-                case '15':
-                    item15(answer.unit);
-                    break;
+        case "12":
+          item12(answer.unit);
+          break;
 
-                case '16':
-                    item16(answer.unit);
-                    break;
+        case "13":
+          item13(answer.unit);
+          break;
 
-                case '17':
-                    item17(answer.unit);
-                    break;
+        case "14":
+          item14(answer.unit);
+          break;
 
-                case '18':
-                    item18(answer.unit);
-                    break;
+        case "15":
+          item15(answer.unit);
+          break;
 
-                case '19':
-                    item19(answer.unit);
-                    break;
-                case '20':
-                    item20(answer.unit);
-                    break;
-            }
-        });
-    }        
-    //Once the customer has placed the order, your application should check if your store has enough of the product to meet the customer's request.
-    //If not, the app should log a phrase like Insufficient quantity!, and then prevent the order from going through.
-    //However, if your store does have enough of the product, you should fulfill the customer's order.
-    // This means updating the SQL database to reflect the remaining quantity.
-    // Once the update goes through, show the customer the total cost of their purchase.
-    function item11(unit) {
-        //Checks database to determine if there is enough units of the product to purchase
-        var query = "SELECT * FROM products WHERE id = 11"; //query table products, stock_quantity
-        var updatedQuantity;
-        connection.query(query, function(err, res) {
-            if (unit <= res[0].stock_quantity) {
-                updatedQuantity = res[0].stock_quantity - unit;
-                console.log("Your cost is: " + (unit * res[0].price));
-                updateStock();
-            }
-            else {
-                return console.log("Insufficient quantity!");
-            }
-        });
-        //Updates stock quantity
-        function updateStock() {
-            var query = "UPDATE products SET stock_quantity = " + updatedQuantity + "WHERE id = 11";
-            connection.query(query, function(err, res) {
-                console.log("Stock Quantity is now: " + updatedQuantity);
-            });
-        }    
+        case "16":
+          item16(answer.unit);
+          break;
+
+        case "17":
+          item17(answer.unit);
+          break;
+
+        case "18":
+          item18(answer.unit);
+          break;
+
+        case "19":
+          item19(answer.unit);
+          break;
+        case "20":
+          item20(answer.unit);
+          break;
+      }
+    });
+}
+//Once the customer has placed the order, your application should check if your store has enough of the product to meet the customer's request.
+//If not, the app should log a phrase like Insufficient quantity!, and then prevent the order from going through.
+//However, if your store does have enough of the product, you should fulfill the customer's order.
+// This means updating the SQL database to reflect the remaining quantity.
+// Once the update goes through, show the customer the total cost of their purchase.
+function item11(unit) {
+  //Checks database to determine if there is enough units of the product to purchase
+  var query = "SELECT * FROM products WHERE id = 11"; //query table products, stock_quantity
+  var updatedQuantity;
+  connection.query(query, function(err, res) {
+    if (unit <= res[0].stock_quantity) {
+      updatedQuantity = res[0].stock_quantity - unit;
+      console.log("Your cost is: " + unit * res[0].price);
+      updateStock();
+    } else {
+      return console.log("Insufficient quantity!");
     }
+  });
+  //Updates stock quantity
+  function updateStock() {
+    var query =
+      "UPDATE products SET stock_quantity = " +
+      updatedQuantity +
+      "WHERE id = 11";
+    connection.query(query, function(err, res) {
+      console.log("Stock Quantity is now: " + updatedQuantity);
+    });
+  }
+}
 // If this activity took you between 8-10 hours, then you've put enough time into this assignment. Feel free to stop here -- unless you want to take on the next challenge.
